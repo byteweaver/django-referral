9c98d61dd1888bf56180ec7732cb7c4361e02109
akuryou <akuryou@akuryou.net>
2012-08-28 23:39:43 +0200

    add some config options

---
+++ referral/settings.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+# Name of the referrer's get parameter name
+GET_PARAMETER = getattr(settings, 'REFERRAL_GET_PARAMETER', 'ref')
+
+# Name of the session variable storing the detected referrer
+SESSION_KEY = getattr(settings, 'REFERRAL_SESSION_KEY', 'referrer')
+
+# Should unknown referrers be auto created? Default: True
+AUTO_CREATE = getattr(settings, 'REFERRAL_AUTO_CREATE', True)
