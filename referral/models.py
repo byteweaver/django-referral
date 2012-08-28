76ae3b536247b4d32624655b6bbe351415fcb285
akuryou <akuryou@akuryou.net>
2012-08-28 23:57:51 +0200

    fix serveral syntax errors in models

--- referral/models.py
+++ referral/models.py
@@ -3,23 +3,23 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Campaign(models.Model):
-    name = models.CharField(_("Name"), max_length=255, unique=True
-    description models.TextField(_("Description"), blank=True, null=True)
+    name = models.CharField(_("Name"), max_length=255, unique=True)
+    description = models.TextField(_("Description"), blank=True, null=True)
 
     class Meta:
-        ordering['name']
+        ordering = ['name']
 
     def __unicode__(self):
         return self.name
 
 class Referrer(models.Model):
     name = models.CharField(_("Name"), max_length=255, unique=True)
-    description models.TextField(_("Description"), blank=True, null=True)
+    description = models.TextField(_("Description"), blank=True, null=True)
     creation_date = models.DateTimeField(_("Creation date"), auto_now_add=True)
-    campaign = models.ForeignKey(_("Campaign"), blank=True, null=True)
+    campaign = models.ForeignKey(Campaign, verbose_name=_("Campaign"), blank=True, null=True)
 
     class Meta:
-        ordering['name']
+        ordering = ['name']
 
     def __unicode__(self):
         return self.name
