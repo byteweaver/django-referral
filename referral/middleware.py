8cf7501030d9c400382d12b12c432b3d69f451d8
akuryou <akuryou@akuryou.net>
2012-08-28 23:46:37 +0200

    add ReferrerMiddleware that will store a detected referrer in session

---
+++ referral/middleware.py
@@ -0,0 +1,19 @@
+import settings
+from models import Referrer
+
+
+class ReferrerMiddleware():
+    def process_request(self, request):
+        if settings.GET_PARAMETER in request.GET:
+            referrer = None
+            referrer_name = request.GET[settings.GET_PARAMETER]
+            try:
+                referrer = Referrer.objects.get(name=referrer_name)
+            except Referrer.DoesNotExist:
+                if settings.AUTO_CREATE:
+                    referrer = Referrer(name=referrer_name)
+                    referrer.save()
+            finally:
+                if not referrer is None:
+                    request.session[settings.SESSION_KEY] = referrer
+
