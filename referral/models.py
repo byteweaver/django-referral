dcb4df824bff3fe10f08e2951f887f422231ff8e
akuryou <akuryou@akuryou.net>
2012-08-31 22:31:47 +0200

    set related_name in Referrer model

--- referral/models.py
+++ referral/models.py
@@ -21,7 +21,7 @@ class Referrer(models.Model):
     name = models.CharField(_("Name"), max_length=255, unique=True)
     description = models.TextField(_("Description"), blank=True, null=True)
     creation_date = models.DateTimeField(_("Creation date"), auto_now_add=True)
-    campaign = models.ForeignKey(Campaign, verbose_name=_("Campaign"), blank=True, null=True)
+    campaign = models.ForeignKey(Campaign, verbose_name=_("Campaign"), related_name='referrers', blank=True, null=True)
 
     class Meta:
         ordering = ['name']
