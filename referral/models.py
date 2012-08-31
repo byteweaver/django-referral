1ae41734485ad9ff61e175b93447e077d6cc5e9c
akuryou <akuryou@akuryou.net>
2012-08-31 21:29:27 +0200

    add UserReferrerManager

--- referral/models.py
+++ referral/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+import settings
+
 
 class Campaign(models.Model):
     name = models.CharField(_("Name"), max_length=255, unique=True)
@@ -25,9 +27,21 @@ class Referrer(models.Model):
     def __unicode__(self):
         return self.name
 
+class UserReferrerManager(models.Manager):
+    def apply_referrer(self, user, request):
+        try:
+            referrer = request.session.pop(settings.SESSION_KEY)
+        except KeyError:
+            pass
+        else:
+            user_referrer = UserReferrer(user=user, referrer=referrer)
+            user_referrer.save()
+
 class UserReferrer(models.Model):
     user = models.OneToOneField(User, verbose_name=_("User"), related_name='referrer')
     referrer = models.ForeignKey(Referrer, verbose_name=_("Referrer"), related_name='users')
+
+    objects = UserReferrerManager()
 
     class Meta:
         ordering = ['referrer__name']
