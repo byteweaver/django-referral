4f0e0e6549cca1292966a45f08e65c50a54653be
akuryou <akuryou@akuryou.net>
2012-08-31 20:48:45 +0200

    add admin for UserReferrer

--- referral/admin.py
+++ referral/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import Campaign, Referrer
+from models import Campaign, Referrer, UserReferrer
 
 class ReferrerInine(admin.TabularInline):
     model = Referrer
@@ -12,6 +12,10 @@ class CampaignAdmin(admin.ModelAdmin):
 class ReferrerAdmin(admin.ModelAdmin):
     list_display = ('name', 'campaign', 'creation_date')
 
+class UserReferrerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'referrer')
+
 admin.site.register(Campaign, CampaignAdmin)
 admin.site.register(Referrer, ReferrerAdmin)
+admin.site.register(UserReferrer, UserReferrerAdmin)
 
