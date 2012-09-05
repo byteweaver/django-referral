1f8d963337339a8b7f3b5dac1f02b86b4a65d741
Akuryou <akuryou@redmoonstudios.de>
2012-09-05 15:01:28 +0200

    add basic ReferrerFactory

--- referral/tests/factories.py
+++ referral/tests/factories.py
@@ -1,11 +1,17 @@
 import factory
 
-from referral.models import Campaign
+from referral.models import Campaign, Referrer
 
 
 class CampaignFactory(factory.Factory):
     FACTORY_FOR = Campaign
 
     name = "Test Campaign"
+    description = "Some long test description"
+
+class ReferrerFactory(factory.Factory):
+    FACTORY_FOR = Referrer
+
+    name = "Test Referrer"
     description = "Some long test description"
 
