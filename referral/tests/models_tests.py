9056febc4c29984bb838dcaae3d209841ed27344
akuryou <akuryou@akuryou.net>
2012-09-05 20:39:47 +0200

    add tests for apply_referrer method

--- referral/tests/models_tests.py
+++ referral/tests/models_tests.py
@@ -1,6 +1,9 @@
+from django.http import HttpRequest
 from django.test import TestCase
 
-from referral.tests.factories import CampaignFactory, ReferrerFactory, UserReferrerFactory
+from referral.models import UserReferrer
+from referral import settings
+from referral.tests.factories import CampaignFactory, ReferrerFactory, UserReferrerFactory, UserFactory
 
 
 class CampaignTestCase(TestCase):
@@ -35,4 +38,24 @@ class UserReferrerTestCase(TestCase):
     def test_model(self):
         obj = UserReferrerFactory()
         self.assertTrue(obj.pk)
+
+    def test_manager_apply_referrer_no_ref(self):
+        user = UserFactory()
+        request = HttpRequest()
+        request.session = {}
+        UserReferrer.objects.apply_referrer(user, request)
+        try:
+            user.user_referrer
+        except UserReferrer.DoesNotExist:
+            pass
+        else:
+            assert False, "Referrer should not exist!"
+
+    def test_manager_apply_referrer(self):
+        referrer = ReferrerFactory()
+        user = UserFactory()
+        request = HttpRequest()
+        request.session = {settings.SESSION_KEY: referrer}
+        UserReferrer.objects.apply_referrer(user, request)
+        self.assertEqual(user.user_referrer.referrer, referrer)
 
