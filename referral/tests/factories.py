e567bbcfe6b8dfcb7159d792ebebd39eae80d429
Akuryou <akuryou@redmoonstudios.de>
2012-09-05 15:08:18 +0200

    make names unique using sequence

--- referral/tests/factories.py
+++ referral/tests/factories.py
@@ -6,12 +6,12 @@ from referral.models import Campaign, Referrer
 class CampaignFactory(factory.Factory):
     FACTORY_FOR = Campaign
 
-    name = "Test Campaign"
+    name = factory.Sequence(lambda n: "Test Campaign %s" % n)
     description = "Some long test description"
 
 class ReferrerFactory(factory.Factory):
     FACTORY_FOR = Referrer
 
-    name = "Test Referrer"
+    name = factory.Sequence(lambda n: "Test Referrer %s" % n)
     description = "Some long test description"
 
