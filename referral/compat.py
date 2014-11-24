0cde022ce8544f15b330ac5325a5cac1c5ecbfb9
akuryou <akuryou@byteweaver.net>
2014-11-24 10:08:16 +0100

    use settings.AUTH_USER_MODEL instead of get_user_model

--- referral/compat.py
+++ referral/compat.py
@@ -7,8 +7,7 @@ AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.USER')
 
 # Django 1.5+ compatibility
 if django.VERSION >= (1, 5):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
+    User = settings.AUTH_USER_MODEL
 else:
     from django.contrib.auth.models import User
 
